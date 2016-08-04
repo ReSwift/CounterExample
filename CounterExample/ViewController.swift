bd5594e9af4879af2a97c13042cee4158bce6ba9
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-08-04 21:52:38 +0100

    Initial Commit

---
+++ CounterExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CounterExample
+//
+//  Created by Colin Eberhardt on 04/08/2016.
+//  Copyright © 2016 Colin Eberhardt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
